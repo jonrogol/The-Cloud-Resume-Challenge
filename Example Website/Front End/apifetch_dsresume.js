d5659564a512ab86f284990062921f6ca1974f88
jonrogol <rogol.jon@gmail.com>
2022-03-07 14:27:53 -0500

    commit

---
+++ Example Website/Front End/apifetch_dsresume.js
@@ -0,0 +1,14 @@
+const countEl = document.getElementById('vc');
+
+updateVisitCount();
+//Testinglllll
+function updateVisitCount() {
+    fetch('https://1nydmobav4.execute-api.us-east-1.amazonaws.com/Prod/visitor_count')
+        .then(response => {
+        return response.json()
+      })
+        .then(data => {
+            console.log(data)
+            document.getElementById('count').innerHTML = data;
+        })
+    }
